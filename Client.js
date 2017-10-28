3ae2c21d80b5727bd8de8a488f21b8babba2038c
Betty <ultrabytes@outlook.fr>
2017-10-29 00:25:05 +0200

    Update Client.js

--- Client.js
+++ Client.js
@@ -43,7 +43,7 @@ bot.on('message', message => {
 			message.reply("What are you trying to do there? :angry:")	
 		}
 		else {
-			message.reply(prefix+"slap USER - Slaps an user >;)")
+			bot.sendMessage(message.channel, prefix+"slap USER - Slaps an user >;)")
 		}
 	}
 	//COMMANDS//
